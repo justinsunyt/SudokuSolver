81195789ea274db2fe1b23f6d2695b4a50cdc07b
Justin Sun <justinsunyt@gmail.com>
2019-12-30 04:01:03 -0800

    added Sudoku Solver Basic.py and solver.py

--- solver.py
+++ solver.py
@@ -46,8 +46,7 @@ def valid(bo, num, pos):
     for i in range(len(bo)):
         if bo[i][pos[1]] == num and pos[0] != i:
             return False
-        
-    #check box
+
     box_x = pos[1] // 3
     box_y = pos[0] // 3
     
